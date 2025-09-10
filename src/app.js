5c83799d403011d1fef46be82626a3bdc149e32a
Thanh <thtto@ucsc.edu>
2025-09-10 15:37:32 -0700

    navMenu complete more buttons added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const toggleMenu = document.getElementById('toggle-menu');
+const navList = document.getElementById('nav-list');
+
+toggleMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+})
